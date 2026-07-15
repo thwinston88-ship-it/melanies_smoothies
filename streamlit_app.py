1af054f908779321ae2144942e80b9e91e9df9bd
Tom <thwinston88@gmail.com>
2026-07-15 15:11:35 -0400

    Correct API call and display smoothie data
    Fix URL formatting and update response handling to display dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,5 +41,6 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
    
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon)  
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothfroot_response.json(), use_container_width=True)
